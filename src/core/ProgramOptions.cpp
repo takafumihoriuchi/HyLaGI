bccb5af0ef121a4681f82b79ed0f9c42f6770975
Takafumi Horiuchi <hawkletter080e@icloud.com>
2018-12-08 18:26:39 +0900

    auto commit from mini-commit

--- src/core/ProgramOptions.cpp
+++ src/core/ProgramOptions.cpp
@@ -162,7 +162,7 @@ void ProgramOptions::init_descriptions()
 void ProgramOptions::parse(int argc, char* argv[])
 {
 	#include <iostream>
-	cout << "=> 3.1:\t in side parsing function" << endl;
+	std::cout << "=> 3.1:\t in side parsing function" << std::endl;
 	positional_options_description positional_opt;
 	positional_opt.add("input-file", -1);
 
