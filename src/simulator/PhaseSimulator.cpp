6b0c2d8d40aecc979173d9a4614d460cf0615c34
Takafumi Horiuchi <hawkletter080e@icloud.com>
2019-01-07 10:40:12 +0900

    auto commit from mini-commit

--- src/simulator/PhaseSimulator.cpp
+++ src/simulator/PhaseSimulator.cpp
@@ -146,16 +146,15 @@ phase_list_t PhaseSimulator::process_todo(phase_result_sptr_t &todo)
 		for (auto it = vm.begin(); it!=vm.end(); ++it) {
 			std::cout << "\t\t=> 5.2.3.1.1:\t variable: " << it->first << "\t: " << it->second << "\n";
 		}
-		// ② 次は「全てのガード条件」を出力する
-		
+		// ② 次は「全てのガード条件」を出力する	
 		std::cout << "\t=> 5.2.3.1.1:\t all the guards in model" << std::endl;
 		std::cout << "\t\t=> 5.2.3.1.1:\t positive asks" << std::endl; // PPで出現
-		for (auto ask : todo->get_diff_positive_asks()) { // askは前件と後件をまとめたもの
-			std::cout << "\t\t\t=> 5.2.3.1.1:\t ask: " << get_infix_string(ask->get_guard()) << "\n";
+		for (auto ask : todo->get_all_positive_asks()) { // askは前件と後件をまとめたもの
+			std::cout << "\t\t\t=> 5.2.3.1.1:\t guard: " << get_infix_string(ask->get_guard()) << "\n";
 		}
 		std::cout << "\t\t=> 5.2.3.1.1:\t negative asks" << std::endl; // IPで出現
-		for (auto ask : todo->get_diff_negative_asks()) {
-			std::cout << "\t\t\t=> 5.2.3.1.1:\t ask: " << get_infix_string(ask->get_guard()) << "\n";
+		for (auto ask : todo->get_all_negative_asks()) {
+			std::cout << "\t\t\t=> 5.2.3.1.1:\t guard: " << get_infix_string(ask->get_guard()) << "\n";
 		}
 		
 		// ③ その後「変数x」に関するガードだけを抜き出す
